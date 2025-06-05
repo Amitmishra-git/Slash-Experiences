8fd69c2e9d13bf6b4ec799e3efbf4217135063e6
Sanu700 <156925758+Sanu700@users.noreply.github.com>
2025-06-05 16:10:31 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ExperienceCRUD from "./components/Admin/ExperienceCRUD";
 import BookingManagement from "./components/Admin/BookingManagement";
 import GoogleSignIn from "./components/Auth/GoogleSignIn";
 import EmailPassword from "./components/Auth/EmailPassword";
+import './App.css';
 
 function App() {
   return (
